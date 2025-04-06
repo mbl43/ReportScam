589c8d351d19c17c210ba6b76934adaaad72fbd4
mangesh <mangeshlemte.a23@gmail.com>
2025-04-06 18:25:54 +0530

    add ReportList and Footer components to App; update main.jsx to include Bootstrap JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import React from "react";
 
-import { Navbar, Hero } from "./components/index";
+import { Navbar, Hero, ReportList, Footer } from "./components/index";
 function App() {
   return (
     <>
       <Navbar />
       <div style={{ height: "4rem" }}></div>
       <Hero />
+      <ReportList/>
+      <Footer/>
     </>
   );
 }
