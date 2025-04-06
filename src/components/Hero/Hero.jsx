feedf2d7645f8f067ede7291b7e34afd506e9dc1
mangesh <mangeshlemte.a23@gmail.com>
2025-04-06 18:26:28 +0530

    refactor Hero component for improved styling and layout consistency

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,16 +4,16 @@ import React from "react";
 const Hero = () => {
   return (
     <main className="container mx-auto">
-      <section style={{ padding: "7rem 0" }}>
+      <section style={{ padding: "7.3rem 0" }}>
         <div className="mx-auto text-center">
           <div>
             <span
-              className="rounded-pill py-1 px-2"
+              className="rounded-pill py-1 px-2 "
               style={{ backgroundColor: "var(--muted)", color: "var(--text)" }}
             >
               Anonymous. Safe. Empowered.
             </span>
-            <h1 className="responsive-heading">
+            <h1 className="responsive-heading mt-2 tracking-in-expand">
              
               &lt; ReportScam /&gt; <br />
               Where people speak up against fraud.
@@ -25,11 +25,11 @@ const Hero = () => {
               <br /> Just honest stories to protect others from the same scams.
             </p>
           </div>
-          <div className="gap-4 d-flex justify-content-center pt-3">
-            <button className="btn btn-light border-1 rounded-pill border-black  p-2 px-4 fw-normal">
+          <div className="pt-3">
+            <button className="btn btn-light border-1 rounded-pill border-black m-1 p-2 px-4 fw-normal">
               Report a Scam
             </button>
-            <button className="btn btn-dark border-0 rounded-pill border-light p-2 px-4 fw-normal">
+            <button className="btn btn-dark border-1 rounded-pill border-white m-1 p-2 px-4 fw-normal">
               Read Real Experiences <ChevronRight size={18} />
             </button>
           </div>
