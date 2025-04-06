8bfbcc975f609fdb6ae942e490b8321a2c5ae023
mangesh <mangeshlemte.a23@gmail.com>
2025-04-06 18:26:34 +0530

    add ReportList component to display user-shared scam reports

---
+++ src/components/ReportList/ReportList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ReportList = () => {
+  return (
+    <section className="container mx-auto" id="browse">
+      <div className="">
+        <div>
+          <h2 className="fs-5">
+            Authentic scam reports shared by people just like you — to warn,
+            protect, and empower others.
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ReportList;
