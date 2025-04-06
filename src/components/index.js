8623a6fd838ead7f65f15b4bf705907eef38d7ae
mangesh <mangeshlemte.a23@gmail.com>
2025-04-06 20:16:43 +0530

    add Reportform component to index.js exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import ReportList from "./ReportList/ReportList";
 import Footer from "./Footer/Footer";
-export {Navbar,Hero,ReportList,Footer}
+import Reportform from "./Reportform/Reportform"
+export {Navbar,Hero,ReportList,Footer,Reportform}
