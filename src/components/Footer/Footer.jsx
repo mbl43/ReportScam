b15231f4eecd0df161658f802c3c304ac989950f
mangesh <mangeshlemte.a23@gmail.com>
2025-04-06 18:26:20 +0530

    add Footer component with copyright information and developer link

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className=" text-center py-3 mt-auto">
+      <p className="mb-0">
+        © 2025 ReportScam. Developed with ❤️ by{" "}
+        <a
+          href="https://mangesh-portfolio-rt.vercel.app/"
+          className="fw-semibold "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mangesh
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
