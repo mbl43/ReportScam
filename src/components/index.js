4cb248fc0bb11ae373646da8748e5982e5901e96
mangesh <mangeshlemte.a23@gmail.com>
2025-07-06 00:50:32 +0530

    feat: add Card component to exports in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Hero from "./Hero/Hero";
 import ReportList from "./ReportList/ReportList";
 import Footer from "./Footer/Footer";
 import Reportform from "./Reportform/Reportform"
-export {Navbar,Hero,ReportList,Footer,Reportform}
+import Card from "./card/Card";
+export {Navbar,Hero,ReportList,Footer,Reportform,Card}
