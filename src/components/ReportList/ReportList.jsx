4441ca8334f5bc9e49fe49bcba97586bfb9798db
mangesh <mangeshlemte.a23@gmail.com>
2025-07-06 00:50:46 +0530

    feat: enhance ReportList component with improved layout and Card integration

--- src/components/ReportList/ReportList.jsx
+++ src/components/ReportList/ReportList.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import {Card} from "../index";
 
 const ReportList = () => {
   return (
     <section className="container mx-auto" id="browse">
-      <div className="">
+      <div className="mb-4">
         <div>
-          <h2 className="fs-5">
+          <h2 className="fs-5 text-center">
             Authentic scam reports shared by people just like you — to warn,
             protect, and empower others.
           </h2>
         </div>
       </div>
+      <Card/>
     </section>
   );
 };
